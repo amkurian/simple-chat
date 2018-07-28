5526f6f3af7db6ecabd6c73e20223adac6d399c3
= <arun.kurian@rubyians.com>
2018-07-28 16:53:24 +0530

    implementing the message io event

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-  console.log();(req.body);
   messages.push(req.body);
-  console.log(messages);
+  io.emit('message', req.body);
   res.sendStatus(200);
 })
 
