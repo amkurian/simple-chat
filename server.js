376f68332d5c46d65f4e38cae4f5d927cfe680db
= <arun.kurian@rubyians.com>
2018-07-28 16:47:21 +0530

    adding socket.io to enable push notifications

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -20,7 +21,11 @@ app.post('/messages', (req, res) => {
   res.sendStatus(200);
 })
 
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
 
-var server = app.listen(3000, () => {
+
+var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
 });
