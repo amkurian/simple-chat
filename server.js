752096daff0f19ca674868713e3182cbfcc20072
= <arun.kurian@rubyians.com>
2018-07-28 21:55:23 +0530

    saving and retreiving the messages from database implemented

--- server.js
+++ server.js
@@ -9,24 +9,34 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [{"name":"arun","message":"hello world"}]
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
 var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
 
 app.get('/messages', (req, res) => {
-  res.send(messages);
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
 })
 
 app.post('/messages', (req, res) => {
-  messages.push(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
 })
 
 io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl , (err) => {
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
   console.log('mongodb connected',err);
 })
 
