07960f2a8ef3f4308e7a9eb88b509536ae3afd3b
= <arun.kurian@rubyians.com>
2018-07-28 21:30:02 +0530

    adding mongoose and connecting to the mlab mongodb

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var bodyParser = require('body-parser')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [{"name":"arun","message":"hello liya"},{"name":"liya","message":"hello arun"}]
+var messages = [{"name":"arun","message":"hello world"}]
+var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
 
 app.get('/messages', (req, res) => {
   res.send(messages);
@@ -24,6 +26,9 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
+mongoose.connect(dbUrl , (err) => {
+  console.log('mongodb connected',err);
+})
 
 var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
