72a8ba41a2c4f9c0548e14164199dfb5c6566724
= <arun.kurian@rubyians.com>
2018-07-28 15:23:35 +0530

    adding the get message functionality

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+var messages = [{"name":"arun","message":"hello liya"},{"name":"liya","message":"hello arun"}]
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+})
 
 var server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port);
