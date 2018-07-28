42c00d17d7507e1106035c218e2cf4fa6268709d
= <arun.kurian@rubyians.com>
2018-07-28 15:54:14 +0530

    adding the send message functionality

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [{"name":"arun","message":"hello liya"},{"name":"liya","message":"hello arun"}]
 
 app.get('/messages', (req, res) => {
   res.send(messages);
 })
+
+app.post('/messages', (req, res) => {
+  console.log();(req.body);
+  messages.push(req.body);
+  console.log(messages);
+  res.sendStatus(200);
+})
+
 
 var server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port);
