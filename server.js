0418a558900b2f10af1972f75782a53e82a7b534
= <arun.kurian@rubyians.com>
2018-07-29 13:42:55 +0530

    improving the code by using async-await

--- server.js
+++ server.js
@@ -23,26 +23,21 @@ app.get('/messages', (req, res) => {
 })
 
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
   var message = new Message(req.body);
-  message.save()
-  .then(() => {
+
+  var savedMessage = await message.save()
     console.log('saved');
-    return Message.findOne({message:'badword'});
-  })
-  .then(censored => {
-    if(censored){
-      console.log('censored words', censored);
-      return Message.remove({_id: censored.id})
-    }
-    io.emit('message', req.body);
+
+  var censored = await Message.findOne({message:'badword'});
+    if(censored)
+      await Message.remove({_id: censored.id})
+    else
+      io.emit('message', req.body);
     res.sendStatus(200);
-  })
-  .catch((err) => {
-    res.sendStatus(500);
-    return console.log(err);
-  })
 })
+
+
 
 io.on('connection', () =>{
   console.log('a user is connected')
